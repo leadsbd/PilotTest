e0dec02c9f9c1383bd86e0f2412052c1c7619925
Saleh Masum <copotron_21@yahoo.com>
2013-05-22 17:22:53 +0600

    ClassA has been added :)

---
+++ TestPilit2/PracticeClassA.h
@@ -0,0 +1,13 @@
+//
+//  PracticeClassA.h
+//  TestPilit2
+//
+//  Created by LEADS on 5/22/13.
+//  Copyright (c) 2013 Leads Corporation Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PracticeClassA : NSObject
+
+@end
---
+++ TestPilit2/PracticeClassA.m
@@ -0,0 +1,13 @@
+//
+//  PracticeClassA.m
+//  TestPilit2
+//
+//  Created by LEADS on 5/22/13.
+//  Copyright (c) 2013 Leads Corporation Limited. All rights reserved.
+//
+
+#import "PracticeClassA.h"
+
+@implementation PracticeClassA
+
+@end
