8d0d3a2fef7606b99f8d3a237a420772fa4be50b
ruma <ruma@rumas-iMac.local>
2013-05-22 17:32:45 +0600

    class added

---
+++ Ruma.h
@@ -0,0 +1,13 @@
+//
+//  Ruma.h
+//  TestPilit2
+//
+//  Created by ruma on 5/22/13.
+//  Copyright (c) 2013 Leads Corporation Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ruma : NSObject
+
+@end
---
+++ Ruma.m
@@ -0,0 +1,13 @@
+//
+//  Ruma.m
+//  TestPilit2
+//
+//  Created by ruma on 5/22/13.
+//  Copyright (c) 2013 Leads Corporation Limited. All rights reserved.
+//
+
+#import "Ruma.h"
+
+@implementation Ruma
+
+@end
