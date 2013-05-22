c7e691a0c318564e201fb791c6ed25c0b0ad8a4e
FarzanaMili <cse07_046@yahoo.com>
2013-05-22 17:10:46 +0600

    Farzana added new class

---
+++ TestPilit2/Farzana.h
@@ -0,0 +1,13 @@
+//
+//  Farzana.h
+//  TestPilit2
+//
+//  Created by Farzana on 5/22/13.
+//  Copyright (c) 2013 Leads Corporation Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Farzana : NSObject
+-(void)print;
+@end
---
+++ TestPilit2/Farzana.m
@@ -0,0 +1,18 @@
+//
+//  Farzana.m
+//  TestPilit2
+//
+//  Created by Farzana on 5/22/13.
+//  Copyright (c) 2013 Leads Corporation Limited. All rights reserved.
+//
+
+#import "Farzana.h"
+
+@implementation Farzana
+
+-(void)print{
+    
+    NSLog(@"I Am Farzana");
+
+}
+@end
