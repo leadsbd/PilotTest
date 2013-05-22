3f7b0433d52a2734d3d65d9163b248481e4fce48
babumirdha <babumirdha@gmail.com>
2013-05-22 16:59:54 +0600

    first commit

--- TestPilit2/AppDelegate.m
+++ TestPilit2/AppDelegate.m
@@ -12,6 +12,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    //sd fiasdf sadf sad
+    
     // Override point for customization after application launch.
     return YES;
 }
